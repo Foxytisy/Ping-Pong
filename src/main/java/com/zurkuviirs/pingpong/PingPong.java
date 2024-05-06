5ac194eba8dff9c8aa4324939581036fed1bae99
Malaclord <56694430+Malaclord@users.noreply.github.com>
2024-05-06 11:46:19 +0200

    refactor

--- src/main/java/com/zurkuviirs/pingpong/PingPong.java
+++ src/main/java/com/zurkuviirs/pingpong/PingPong.java
@@ -12,34 +12,30 @@ import org.slf4j.LoggerFactory;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 
 public class PingPong implements ClientModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("pingpong");
-
 	@Override
 	public void onInitializeClient() {
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-			dispatcher.register(ClientCommandManager.literal("pingpong")
-					.executes(context -> {
-						if (MinecraftClient.getInstance().player != null)
-							MinecraftClient.getInstance().player.sendMessage(Text.literal("Ping Pong! 🏓"), false);
-						return 1;
-					})
-					.then(ClientCommandManager.argument("pongablemessage", StringArgumentType.greedyString())
-							.executes(context -> {
-								String message = StringArgumentType.getString(context, "pongablemessage");
-								final String pongedMessage = getPongedMessage(message);
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> dispatcher.register(ClientCommandManager.literal("pingpong")
+                .executes(context -> {
+                    if (MinecraftClient.getInstance().player != null)
+                        MinecraftClient.getInstance().player.sendMessage(Text.literal("Ping Pong! 🏓"), false);
+                    return 1;
+                })
+                .then(ClientCommandManager.argument("pongablemessage", StringArgumentType.greedyString())
+                        .executes(context -> {
+                            String message = StringArgumentType.getString(context, "pongablemessage");
+                            final String pongedMessage = getPongedMessage(message);
 
-								//MinecraftClient client = MinecraftClient.getInstance();
-								//if (client.player != null) {
-								//	client.inGameHud.getChatHud().addMessage(Text.literal((pongedMessage + " :ping_pong:")));
-								//}
+                            //MinecraftClient client = MinecraftClient.getInstance();
+                            //if (client.player != null) {
+                            //	client.inGameHud.getChatHud().addMessage(Text.literal((pongedMessage + " :ping_pong:")));
+                            //}
 
-								if (MinecraftClient.getInstance().currentScreen instanceof ChatScreen)
-									((ChatScreen)MinecraftClient.getInstance().currentScreen).sendMessage((pongedMessage + " 🏓"),true);
-								return 1;
-							})
-					)
-			);
-		});
+                            if (MinecraftClient.getInstance().currentScreen instanceof ChatScreen)
+                                ((ChatScreen)MinecraftClient.getInstance().currentScreen).sendMessage((pongedMessage + " 🏓"),true);
+                            return 1;
+                        })
+                )
+        ));
 }
 
 	private static @NotNull String getPongedMessage(String message) {
