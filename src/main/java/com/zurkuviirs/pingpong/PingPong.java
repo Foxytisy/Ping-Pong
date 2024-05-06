ffbe272935ed9292c16def27021e835a9067e9ec
Malaclord <56694430+Malaclord@users.noreply.github.com>
2024-05-06 11:45:43 +0200

    refactor

--- src/main/java/com/zurkuviirs/pingpong/PingPong.java
+++ src/main/java/com/zurkuviirs/pingpong/PingPong.java
@@ -6,6 +6,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.text.Text;
 import net.fabricmc.api.ClientModInitializer;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
@@ -18,38 +19,45 @@ public class PingPong implements ClientModInitializer {
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
 			dispatcher.register(ClientCommandManager.literal("pingpong")
 					.executes(context -> {
-						MinecraftClient.getInstance().player.sendMessage(Text.literal("Ping Pong! 🏓"), false);
+						if (MinecraftClient.getInstance().player != null)
+							MinecraftClient.getInstance().player.sendMessage(Text.literal("Ping Pong! 🏓"), false);
 						return 1;
 					})
 					.then(ClientCommandManager.argument("pongablemessage", StringArgumentType.greedyString())
 							.executes(context -> {
 								String message = StringArgumentType.getString(context, "pongablemessage");
-								StringBuilder pongedMessageBuilder = new StringBuilder();
-								int pongLength = message.length();
-								for (int i = 0; i < pongLength / 2; i++) {
-									char pongStart = message.charAt(i);
-									pongedMessageBuilder.append(pongStart);
-									char pongEnd = message.charAt((pongLength - 1) - i);
-									pongedMessageBuilder.append(pongEnd);
-								}
-								if (pongLength % 2 != 0) {
-									char middleChar = message.charAt(pongLength / 2);
-									pongedMessageBuilder.append(middleChar);
-								}
-
-								String pongedMessage = pongedMessageBuilder.toString();
+								final String pongedMessage = getPongedMessage(message);
 
 								//MinecraftClient client = MinecraftClient.getInstance();
 								//if (client.player != null) {
 								//	client.inGameHud.getChatHud().addMessage(Text.literal((pongedMessage + " :ping_pong:")));
 								//}
 
-								((ChatScreen)MinecraftClient.getInstance().currentScreen).sendMessage((pongedMessage + " :ping_pong:"),true);
+								if (MinecraftClient.getInstance().currentScreen instanceof ChatScreen)
+									((ChatScreen)MinecraftClient.getInstance().currentScreen).sendMessage((pongedMessage + " 🏓"),true);
 								return 1;
 							})
 					)
 			);
 		});
 }
+
+	private static @NotNull String getPongedMessage(String message) {
+		StringBuilder pongedMessageBuilder = new StringBuilder();
+
+		int pongLength = message.length();
+		for (int i = 0; i < pongLength / 2; i++) {
+			char pongStart = message.charAt(i);
+			pongedMessageBuilder.append(pongStart);
+			char pongEnd = message.charAt((pongLength - 1) - i);
+			pongedMessageBuilder.append(pongEnd);
+		}
+		if (pongLength % 2 != 0) {
+			char middleChar = message.charAt(pongLength / 2);
+			pongedMessageBuilder.append(middleChar);
+		}
+
+        return pongedMessageBuilder.toString();
+	}
 
 }
